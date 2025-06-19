ea5a12ae436a3a082e8f879f3c5a934b8b11b8d5
VuJason <minhtrong220703@gmail.com>
2025-06-19 12:02:25 +0700

    character animaiton and design enemies, boss

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -3,9 +3,18 @@
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    public float runSpeed = 5f; // tốc độ chạy
+    [SerializeField]
+    private float speed = 5f; // tốc độ chạy
     private bool isFacingRight = true; // trạng thái hướng mặt nhân vật
-    public Animator anim;
+    [SerializeField]
+    private Animator animator;
+
+    private Vector2 screenBound;
+
+    private void Start()
+    {
+        screenBound = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+    }
 
     void Update()
     {
@@ -14,19 +23,28 @@ public class PlayerMovement : MonoBehaviour
         float moveY = Input.GetAxis("Vertical");
 
         // Tạo vector di chuyển 2 chiều
-        Vector3 movement = new Vector3(moveX, moveY, 0) * runSpeed * Time.deltaTime;
+        Vector3 movement = new Vector3(moveX, moveY, 0) * speed * Time.deltaTime;
         transform.Translate(movement);
 
+       if(moveX != 0)
+        {
+            animator.SetBool("1_Move", true);
+        }else
+        {
+            animator.SetBool("1_Move", false);
+        }
+
+
         // Lật nhân vật nếu đi trái/phải
-        if (moveX > 0 && !isFacingRight)
+        if (moveX < 0 && !isFacingRight)
         {
             Flip();
         }
-        else if (moveX < 0 && isFacingRight)
+        else if (moveX > 0 && isFacingRight)
         {
             Flip();
         }
-        anim.SetFloat("di chuyen", Mathf.Abs(moveX));
+        animator.SetFloat("di chuyen", Mathf.Abs(moveX));
     }
 
 
@@ -37,5 +55,10 @@ public class PlayerMovement : MonoBehaviour
         Vector3 localScale = transform.localScale;
         localScale.x *= -1;
         transform.localScale = localScale;
+    }
+
+    void HandleMovement()
+    {
+
     }
 }
